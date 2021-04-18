bbe3e8db312191d75bef259931d129e24dae797e
ZhangHui <zhanghui_china2020@163.com>
2021-04-18 22:40:04 +0800

    add third homework

---
+++ training_camp/task_3_zhanghui_china/profiling/run_msprof.sh
@@ -0,0 +1,4 @@
+python3.7.5 /home/HwHiAiUser/Ascend/ascend-toolkit/20.2.alpha001/x86_64-linux/toolkit/tools/profiler/profiler_tool/analysis/msprof/msprof.pyc import -dir $1
+python3.7.5 /home/HwHiAiUser/Ascend/ascend-toolkit/20.2.alpha001/x86_64-linux/toolkit/tools/profiler/profiler_tool/analysis/msprof/msprof.pyc query -dir $1
+python3.7.5 /home/HwHiAiUser/Ascend/ascend-toolkit/20.2.alpha001/x86_64-linux/toolkit/tools/profiler/profiler_tool/analysis/msprof/msprof.pyc export timeline -dir $1
+python3.7.5 /home/HwHiAiUser/Ascend/ascend-toolkit/20.2.alpha001/x86_64-linux/toolkit/tools/profiler/profiler_tool/analysis/msprof/msprof.pyc export summary -dir $1
